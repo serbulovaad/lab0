2b458ff39986246012561a7deae6ff4f23a7c349
crazzyfuncat <s.alla.d@yandex.ru>
2024-09-12 00:04:55 +0300

    made hello and if

--- Serbulova/main.cpp
+++ Serbulova/main.cpp
@@ -4,5 +4,18 @@ using namespace std;
 int main() 
 {
 	cout << "Hello world" << endl;
+	cout << "Print 10: ";
+	int x;
+	cin >> x;
+
+	if (x == 10)
+		cout << "Good job" << endl;
+	else
+		cout << "Error!\nx = " << x << endl;
+
 	return 0;
+
+	if (1) // не запускается т.к return выше
+		cout << "End" << endl;
+
 }
